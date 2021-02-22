faaf8a9c9193fbde35c03cef4ea2e654166dcfa4
oziomaogbe <ogbeoziomajnr@yahoo.com>
2021-02-22 08:47:15 +0100

    Add tests for data conversion

--- app/src/test/java/com/ozioma/bitcoininfo/helper/DataHelperTest.kt
+++ app/src/test/java/com/ozioma/bitcoininfo/helper/DataHelperTest.kt
@@ -1,11 +1,30 @@
 package com.ozioma.bitcoininfo.helper
 
+import com.ozioma.bitcoininfo.model.UiMarketPriceData
+import com.ozioma.bitcoininfo.model.UiMarketPriceValue
+import com.ozioma.data.data.MarketPriceData
+import com.ozioma.data.data.MarketPriceValues
+import org.junit.Assert
 import org.junit.Test
 
 class DataHelperTest {
 
     @Test
-    fun marketPriceDataToUIMarketPriceData() {
-        val datahelper = DataHelper()
+    fun `marketPriceDataToUIMarketPriceData conversion works correctly`() {
+        val dataHelper = DataHelper()
+        val marketPriceData = MarketPriceData(
+            status = "ok", name = "Market Price",
+            unit = "NGN", period = "1 year",
+            description = "nothing", values = listOf(MarketPriceValues(1611360000L, 33002.38))
+        )
+        val uiMarketPrice = dataHelper.marketPriceDataToUIMarketPriceData(marketPriceData)
+        val expectedUiMarketPrice = UiMarketPriceData(
+            marketPriceData.name,
+            marketPriceData.period,
+            marketPriceData.values.map {
+                UiMarketPriceValue(it.timeStamp, it.value)
+            })
+        Assert.assertEquals(expectedUiMarketPrice.toString(), uiMarketPrice.toString())
     }
+
 }
--- app/src/test/java/com/ozioma/bitcoininfo/model/UiMarketPriceDataTest.kt
+++ app/src/test/java/com/ozioma/bitcoininfo/model/UiMarketPriceDataTest.kt
@@ -1,18 +1,61 @@
 package com.ozioma.bitcoininfo.model
 
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class UiMarketPriceDataTest {
 
     @Test
     fun `getLatestMarketPrice returns correct value`() {
-        assert(false)
+        val uiMarketPriceData = UiMarketPriceData(
+            "Market Price",
+            "1 year",
+            listOf(UiMarketPriceValue(1611360000L, 33002.38), UiMarketPriceValue(161136L, 33.38))
+        )
+        assertEquals(UiMarketPriceValue(161136L, 33.38), uiMarketPriceData.getLatestMarketPrice())
     }
 
     @Test
     fun toMpChartLineData() {
-        assert(false)
+        val uiMarketPriceData = UiMarketPriceData(
+            "Market Price",
+            "1 year",
+            listOf(UiMarketPriceValue(1611360000L, 33002.38), UiMarketPriceValue(161136L, 33.38))
+        )
+        val label = "Market Price"
+        val expectedLineData = LineData(
+            LineDataSet(
+                listOf(
+                    Entry(
+                        1611360000L.toFloat(),
+                        33002.38F
+                    )
+                ), label
+            )
+        )
+        val actualLineData = uiMarketPriceData.toMpChartLineData(label)
+        assertTrue(lineDataAreEqual(expectedLineData, actualLineData))
+    }
+
+    private fun lineDataAreEqual(lineData1: LineData, lineData2: LineData): Boolean {
+        var hasMismatchedItems = false
+        val datasetEntryCount = lineData1.dataSets.first().entryCount
+        for (index in 0 until datasetEntryCount) {
+            if (lineData1.dataSets.first().getEntryForIndex(index).x != lineData2.dataSets.first()
+                    .getEntryForIndex(index).x || lineData1.dataSets.first()
+                    .getEntryForIndex(index).y != lineData2.dataSets.first()
+                    .getEntryForIndex(index).y
+            ) {
+                hasMismatchedItems = true
+            }
+        }
+        return lineData1.dataSetLabels.contentDeepEquals(lineData2.dataSetLabels) && !hasMismatchedItems
     }
 }
